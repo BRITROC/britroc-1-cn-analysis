cf5f8680cc3ce5971b44cdbeb8ba820549d23fee
Philip Smith <philsmith990@hotmail.co.uk>
2022-03-30 18:17:48 +0100

    Analysis progression
    Addition of more verbose ploidy table, improved signature plots, altered main figure layouts, fixed loading of new ploidy table, partial code comments on clinical data

--- compile_plots.R
+++ compile_plots.R
@@ -4,7 +4,7 @@ library(ggrepel)
 library(ggpubr)
 library(cowplot)
 
-## Empty placeholder
+## Empty place holder
 empty <- ggplot() +
           theme_bw() +
           theme(panel.border = element_blank())
@@ -19,22 +19,30 @@ focal_rates_clinic <- readRDS("copy_number_analysis/focal_analysis/plots/amp_del
 focal_rates_clinic <- focal_rates_clinic + theme(legend.position = "bottom")
 
 ## Load signature plots 
-# signature_stacked <- readRDS("copy_number_signatures/plots/signatures_stacked_bar.RDS")
-# signature_stacked <- signature_stacked +
-#                       theme(legend.position = "none")
 signature_stacked_all <- readRDS("copy_number_signatures/plots/signatures_allSamples_stacked_bar.RDS")
 signature_stacked_all <- signature_stacked_all + theme(legend.position = "none")
 
-# sig_box_paired_wilcox <- readRDS("copy_number_signatures/plots/sig_box_paired_wilcox.RDS")
-# sig_box_paired_wilcox <- sig_box_paired_wilcox +
-#                           labs(title = "",caption = "")
-
 sig_box_wilcox <- readRDS("copy_number_signatures/plots/sig_box_wilcox.RDS")
-sig_box_wilcox <- sig_box_wilcox + labs(title = "",caption = "") + theme(legend.position = "bottom")
+sig_box_wilcox <- sig_box_wilcox + labs(title = "",caption = "") + theme(legend.position = "none")
                         
+sig_box_paired_wilcox <- readRDS("copy_number_signatures/plots/sig_box_paired_wilcox.RDS")
+sig_box_paired_wilcox <- sig_box_paired_wilcox +
+    labs(title = "",caption = "") + theme(legend.position = "none")
 
 signature_stacked_alt <- readRDS("copy_number_signatures/plots/signatures_stacked_bar_alt.RDS")
-signature_stacked_alt <- signature_stacked_alt + theme(legend.position = "bottom")
+signature_stacked_alt <- signature_stacked_alt + theme(legend.position = "none")
+
+sig_legend_a <- get_legend(
+    signature_stacked_all + 
+        guides(color = guide_legend(nrow = 1)) +
+        theme(legend.position = "bottom")
+)
+
+sig_legend_b <- get_legend(
+    sig_box_wilcox + 
+        guides(color = guide_legend(nrow = 1)) +
+        theme(legend.position = "bottom")
+)
 
 ## Load tissue plots
 cna_tissue_rates <- readRDS("copy_number_analysis/sites_of_relapse/plots/cna_rates_tissue.RDS")
@@ -48,6 +56,7 @@ tissue_legend_b <- get_legend(
     theme(legend.position = "bottom")
 )
 cna_tissue_ith <- cna_tissue_ith + theme(legend.position = "none")
+
 ## Figure 1
 # Thomas
 
@@ -59,19 +68,28 @@ fig3 <- plot_grid(substraction_plot,focal_rates_clinic,labels = c("A","B"),nrow 
 ggsave2(filename = "figure_3_render.png",plot = fig3,width = 11,height = 8,units = "in",dpi = 300)  
 
 ## Figure 4
-fig4 <- plot_grid(signature_stacked_all,plot_grid(signature_stacked_alt,sig_box_wilcox,ncol = 2,labels = c("B","C")),
-                  nrow = 2,
-                  rel_heights = c(0.4,0.6),
-                  labels = c("A",""))
+
+fig4 <- plot_grid(signature_stacked_all,sig_box_wilcox,plot_grid(sig_legend_a,sig_legend_b,ncol = 2,labels = c("","")),
+                  nrow = 3,
+                  rel_heights = c(1,1,0.1),
+                  labels = c("A","B"))
 ggsave2(filename = "figure_4_render.png",plot = fig4,width = 10,height = 11,units = "in",dpi = 300)  
 
 ## Figure 5
-fig5 <- plot_grid(cna_tissue_rates,
+fig5 <- plot_grid(plot_grid(signature_stacked_alt,
+                  plot_grid(sig_box_paired_wilcox,empty,nrow = 2,labels = c("B","C")),
+                  ncol = 2,
+                  labels = c("A","")),nrow = 2,
+                  plot_grid(sig_legend_a,sig_legend_b,ncol = 2,labels = c("","")),rel_heights = c(1,0.1))
+ggsave2(filename = "figure_5_render.png",plot = fig5,width = 10,height = 9,units = "in",dpi = 300)  
+
+## Figure 6
+fig6 <- plot_grid(cna_tissue_rates,
           cna_tissue_counts,
           plot_grid(cna_tissue_ith,tissue_legend_b,labels = c("C","")),
           nrow = 3,
           labels = c("A","B",""),rel_heights = c(1,1.2,0.7))
-ggsave2(filename = "figure_5_render.png",plot = fig5,width = 10,height = 10,units = "in",dpi = 300)
+ggsave2(filename = "figure_6_render.png",plot = fig5,width = 10,height = 10,units = "in",dpi = 300)
 
 ## Supplemental - TCGA-Britroc focal rate comparison 
 britroc_tcga_comp_raw <- readRDS("copy_number_analysis/focal_analysis/plots/britroc_tcga_comp_raw.RDS")
