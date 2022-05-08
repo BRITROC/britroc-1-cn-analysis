7f17915bae64a7017779adfa2b9197f5cfa001f6
Philip Smith <philsmith990@hotmail.co.uk>
2022-05-09 00:40:50 +0100

    tissue analysis completion
    preliminary completion of tissue analysis and addition/correction of required supplemental plots.

--- compile_plots.R
+++ compile_plots.R
@@ -49,11 +49,23 @@ sig_legend_b <- get_legend(
 
 ## Load tissue plots
 cna_tissue_rates <- readRDS("copy_number_analysis/sites_of_relapse/plots/cna_rates_tissue.RDS")
-cna_tissue_rates <- cna_tissue_rates + theme(legend.position = "none")
+cna_tissue_rates <- cna_tissue_rates + theme(legend.position = "none",
+                                             axis.text.x = element_text(angle = 90,hjust = 1,vjust = 0.5))
 cna_tissue_counts <- readRDS("copy_number_analysis/sites_of_relapse/plots/cna_count_tissue.RDS")
-cna_tissue_counts <- cna_tissue_counts + theme(legend.position = "none",axis.text = element_text(angle = 90))
+cna_tissue_counts <- cna_tissue_counts + theme(legend.position = "none",
+                                               axis.ticks.x = element_blank(),
+                                               axis.title.x = element_blank(),
+                                               axis.text.x = element_blank())
 cna_tissue_ith <- readRDS("copy_number_analysis/sites_of_relapse/plots/ith_tissue.RDS")
-cna_tissue_ith <- cna_tissue_ith + theme(legend.position = "none")
+cna_tissue_ith <- cna_tissue_ith + theme(legend.position = "none",
+                                         axis.ticks.x = element_blank(),
+                                         axis.title.x = element_blank(),
+                                         axis.text.x = element_blank())
+cna_tissue_sigs <- readRDS("copy_number_analysis/sites_of_relapse/plots/signature_tissue.RDS")
+cna_tissue_sigs <- cna_tissue_sigs + theme(legend.position = "none",
+                                           axis.ticks.x = element_blank(),
+                                           axis.title.x = element_blank(),
+                                           axis.text.x = element_blank()) 
 
 tissue_legend_a <- get_legend(
   cna_tissue_counts + 
@@ -62,6 +74,28 @@ tissue_legend_a <- get_legend(
 )
 tissue_legend_b <- get_legend(
   cna_tissue_rates + 
+    guides(color = guide_legend(nrow = 1)) +
+    theme(legend.position = "bottom")
+)
+
+## Load supplemental tissue plots
+cn_tissue_counts_strat <- readRDS("copy_number_analysis/sites_of_relapse/plots/cn_count_tissue.RDS")
+cn_tissue_counts_strat <- cn_tissue_counts_strat + theme(legend.position = "none",axis.title.x = element_blank(),
+                                                         axis.ticks.x = element_blank(),
+                                                         axis.text.x = element_blank())
+cn_tissue_sigs_strat <- readRDS("copy_number_analysis/sites_of_relapse/plots/cn_sigs_tissue.RDS")
+cn_tissue_sigs_strat <- cn_tissue_sigs_strat + theme(legend.position = "none",
+                                                     axis.ticks.x = element_blank(),
+                                                     axis.title.x = element_blank(),
+                                                     axis.text.x = element_blank())
+cn_tissue_ith_strat <- readRDS("copy_number_analysis/sites_of_relapse/plots/ith_tissue_strat.RDS")
+cn_tissue_ith_strat <- cn_tissue_ith_strat + theme(legend.position = "none",
+                                                   axis.ticks.x = element_blank(),
+                                                   axis.title.x = element_blank(),
+                                                   axis.text.x = element_blank())
+
+tissue_legend_strat <- get_legend(
+  cn_tissue_ith_strat + 
     guides(color = guide_legend(nrow = 1)) +
     theme(legend.position = "bottom")
 )
@@ -96,15 +130,6 @@ fig5 <- plot_grid(plot_grid(signature_paired_line,
                   plot_grid(sig_legend_a,sig_legend_b,ncol = 2,labels = c("","")),rel_heights = c(1,0.1))
 ggsave2(filename = "plots/figure_5_render.png",plot = fig5,width = 10,height = 9,units = "in",dpi = 300)  
 
-## Figure 6
-fig6 <- plot_grid(
-          plot_grid(cna_tissue_rates,
-                    cna_tissue_counts,align = "hv",
-                    labels = c("A","B")),
-          plot_grid(tissue_legend_b,tissue_legend_a),
-          nrow = 2,rel_heights = c(1,0.1))
-ggsave2(filename = "plots/figure_6_render.png",plot = fig6,width = 10,height = 10,units = "in",dpi = 300)
-
 ## Supplemental - TCGA-Britroc focal rate comparison 
 britroc_tcga_comp_raw <- readRDS("copy_number_analysis/focal_analysis/plots/britroc_tcga_comp_raw.RDS")
 britroc_tcga_comp_raw <- britroc_tcga_comp_raw +
@@ -114,3 +139,22 @@ britroc_tcga_comp_norm <- britroc_tcga_comp_norm +
                            labs(title = "") + theme(legend.position = "bottom")
 sup_plot_britroc_tcga_rates <- plot_grid(britroc_tcga_comp_raw,britroc_tcga_comp_norm,nrow = 2,labels = c("A","B"))
 ggsave2(filename = "plots/supplemental_figure_tcga_britroc_rates.png",plot = sup_plot_britroc_tcga_rates,width = 8,height = 8,units = "in",dpi = 300)  
+
+## Supplemental - tissue 1
+tissue_supplemental1 <- plot_grid(plot_grid(plot_grid(cna_tissue_counts,labels = c("A",""),
+                              plot_grid(cna_tissue_rates,cna_tissue_ith,labels = c("B","C"),ncol = 1,rel_heights = c(0.8,0.2))),
+                                cna_tissue_sigs,nrow = 2,rel_heights = c(0.8,0.2)),
+                              plot_grid(tissue_legend_b,tissue_legend_a),nrow = 2,rel_heights = c(1,0.1))
+
+ggsave2(filename = "plots/tissue_supplemental1.png",plot = tissue_supplemental1,width = 10,height = 12,units = "in",dpi = 300)
+
+## Supplemental - tissue 2
+tissue_supplemental2 <- plot_grid(plot_grid(cn_tissue_counts_strat,
+                    plot_grid(cn_tissue_sigs_strat,cn_tissue_ith_strat,nrow = 2,labels = c("B","C")),
+                    ncol = 2),
+          tissue_legend_strat,
+          labels = c("A",""),
+          nrow = 2,
+          rel_heights = c(1,0.1))
+
+ggsave2(filename = "plots/tissue_supplemental2.png",plot = tissue_supplemental2,width = 10,height = 12,units = "in",dpi = 300)  
