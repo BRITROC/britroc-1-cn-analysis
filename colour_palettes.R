7c89f576440f753d6e339e71f50bd11209a21f67
Philip Smith <philsmith990@hotmail.co.uk>
2022-06-27 17:19:58 +0100

    Updated focal analysis
    Finalised recovery of lost pt status analysis. Reordered focal analysis. Additional patient-specific change plot. Removed redundant nbclust function

--- colour_palettes.R
+++ colour_palettes.R
@@ -8,7 +8,11 @@ loss_colour_setting <- "dodgerblue1"
 del_colour_setting <- "dodgerblue4"
 resistant_colour_setting <- "#F8766D"
 sensitive_colour_setting <- "#00BFC4"
-
+prior_lines_colour_1_setting <- "#ffffcc"
+prior_lines_colour_2_setting <- "#c2e699"
+prior_lines_colour_3_setting <- "#78c679"
+prior_lines_colour_4_setting <- "#238443"
+  
 colour_palettes <- list(arx_rlps=c(arx=primary_colour_setting,
                                    rlps=relapse_colour_setting),
                         diagnosis_relapse=c(diagnosis=primary_colour_setting,
@@ -28,6 +32,10 @@ colour_palettes <- list(arx_rlps=c(arx=primary_colour_setting,
                                    LOSS=loss_colour_setting,
                                    DEL=del_colour_setting),
                         resist_sensitive=c(resistant=resistant_colour_setting,
-                                           sensitive=sensitive_colour_setting))
+                                           sensitive=sensitive_colour_setting),
+                        prior_lines=c("1"=prior_lines_colour_1_setting,
+                                      "2"=prior_lines_colour_2_setting,
+                                      "3"=prior_lines_colour_3_setting,
+                                      "4"=prior_lines_colour_4_setting))
 
 rm(list = ls()[grep("*_colour_setting",ls())])
